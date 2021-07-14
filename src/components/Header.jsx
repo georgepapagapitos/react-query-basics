a4931c7d40c36ac399bc6526b8fda6125771f457
George Papagapitos <george.g.papagapitos@gmail.com>
2021-07-14 15:18:06 -0500

    added scss + css modules

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Header.module.scss';
+
+export default function Header() {
+  return (
+    <div>
+      <h1 className={styles.header}>Header component</h1>
+    </div>
+  );
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from 'react'
 import { useIsFetching } from 'react-query';
 import CreateUser from '../components/CreateUser';
-import User from '../components/User'
-import Users from '../components/Users'
+import User from '../components/User';
+import Users from '../components/Users';
+import styles from '../App.module.scss';
+import Header from '../components/Header';
 
 export default function Dashboard() {
 
   const [selectedUser, setSelectedUser] = useState(null);
   const isFetching = useIsFetching();
 
+  const headerText = "This is a header";
+
   return (
     <>
+      <Header />
+      <h1 className={headerText.length > 5 ? styles.header : null}>{headerText}</h1>
+
       <Users setSelectedUser={setSelectedUser} />
-
       <hr />
-
       <User selectedUser={selectedUser} />
-
       <hr />
-
       <CreateUser />
-
       {isFetching ? <p style={{ padding: '1rem', position: 'fixed', top: '2%', right: '0%' }}>Loading...</p> : null}
     </>
   );
